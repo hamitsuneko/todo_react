495cb92f7dafe76bec5fe53f225e52c4b2ba1fe2
hamitsuneko <hamitsuneko@gmail.com>
2022-11-23 22:14:10 +0300

    Rename navbar

--- src/navbar.js
+++ src/navbar.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import s from './navbar.module.css'
 
-const style = `${s.aaa} navbar-brand`
+const style = `${s.navbar} navbar-brand`
 
 
 export default function Navbar() {
